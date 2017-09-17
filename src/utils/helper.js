72152af3e58efd761af8d29b90486907b72b9430
phongnhung <appstoregugle2310@gmail.com>
2017-09-17 11:03:46 +0700

    Defined a simple encrypt/decrypt func

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,8 +1,133 @@
 'use strict';
-export function helper() {
+function helper() {
     return {
         getUniqueKey: function () {
             return new Date().getTime().toString() + Math.random().toString();
+        },
+        computeCounter: function (str) {
+            let counters = []
+            if (!str || typeof (str) !== "string") {
+                counters.push(0);
+            }
+            let strNo = "";
+            for (let i in str) {
+                strNo += str[i].charCodeAt().toString(16);
+            }
+            for (let i in strNo) {
+                if (!isNaN(strNo[i])) {
+                    const num = parseInt(strNo[i]), pos = counters.indexOf(num);
+                    if (pos >= 0) {
+                        counters[pos] += num;
+                    } else {
+                        counters.push(num);
+                    }
+                }
+            }
+            return counters;
         }
     };
+}
+function encrypt(plainText, seed, reverse, rangeStrs) {
+    if (!plainText || !seed || typeof (plainText) !== "string" || typeof (seed) !== "string") {
+        return "Hello, the world.";
+    }
+    let strLen = plainText.length,
+        counters = helper().computeCounter(seed);
+    let cipherText = "";
+    if (reverse) {
+        for (let i = strLen - 1; i >= 0; i--) {
+            let strPos = rangeStrs.indexOf(plainText[i]),
+                counter = cipherText.length;
+            while (counters[counter] === undefined) {
+                counter--;
+            }
+            let strPosNext = strPos + counters[counter];
+            if (strPos >= 0) {
+                while (strPosNext >= rangeStrs.length) {
+                    strPosNext -= rangeStrs.length;
+                }
+                cipherText += rangeStrs.substr(strPosNext - 1, 1);
+            } else {
+                cipherText += plainText[i];
+            }
+        }
+    } else {
+        for (let i = 0; i <= strLen - 1; i++) {
+            let strPos = rangeStrs.indexOf(plainText[i]),
+                counter = cipherText.length;
+            while (counters[counter] === undefined) {
+                counter--;
+            }
+            let strPosNext = strPos - counters[counter];
+            if (strPos >= 0) {
+                while (strPosNext < 0) {
+                    strPosNext = rangeStrs.length + strPosNext;
+                }
+                cipherText += rangeStrs.substr(strPosNext - 1, 1);
+            } else {
+                cipherText += plainText[i];
+            }
+        }
+    }
+    return cipherText;
+}
+function decrypt(cipherText, seed, reverse, rangeStrs) {
+    if (!cipherText || !seed || typeof (cipherText) !== "string" || typeof (seed) !== "string") {
+        return "Hello, the world.";
+    }
+    let plainText = "",
+        strLen = cipherText.length,
+        counters = helper().computeCounter(seed);
+    if (reverse) {
+        let counter = cipherText.length;
+        for (let i = strLen - 1; i >= 0; i--) {
+            let strPos = rangeStrs.indexOf(cipherText[i]);
+            console.log("Counter truoc=", counter);
+            if (counter > 0) {
+                counter--;
+                while (counters[counter] === undefined && counter > 0) {
+                    counter--;
+                    console.log("Counter=", counter);
+                }
+            }
+            let strPosNext = strPos - counters[counter];
+            if (strPos >= 0) {
+                while (strPosNext < 0) {
+                    strPosNext = rangeStrs.length + strPosNext;
+                    console.log("strPosNext=", strPosNext);
+                }
+                if (strPosNext >= rangeStrs.length - 1) {
+                    strPosNext = 0;
+                } else {
+                    strPosNext++;
+                }
+                plainText += rangeStrs.substr(strPosNext, 1);
+            } else {
+                plainText += cipherText[i];
+            }
+        }
+    } else {
+        for (let i = 0; i <= strLen - 1; i++) {
+            let strPos = rangeStrs.indexOf(cipherText[i]),
+                counter = plainText.length;
+            while (counters[counter] === undefined) {
+                counter--;
+            }
+            let strPosNext = strPos + counters[counter];
+            if (strPos >= 0) {
+                while (strPosNext >= rangeStrs.length) {
+                    strPosNext -= rangeStrs.length;
+                }
+                if (strPosNext >= rangeStrs.length - 1) {
+                    strPosNext = 0;
+                } else {
+                    strPosNext++;
+                }
+                plainText += rangeStrs.substr(strPosNext, 1);
+            } else {
+                plainText += cipherText[i];
+            }
+        }
+    }
+    return plainText;
 }
